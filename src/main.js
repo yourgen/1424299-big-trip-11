4748b67e9227f7327574e546133e1b25d49a3b23
Yuriy <yourgen.edu@mail.ru>
2020-04-01 13:32:00 +0300

    stage 2
    Меню и фильтры

--- src/main.js
+++ src/main.js
@@ -24,15 +24,52 @@ const createTripCostTemplate = () => {
   );
 };
 
+const createSiteMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
+
+const createFilterTemplate = () => {
+  return (
+    `<form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`
+  );
+};
+
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElem = document.querySelector('.trip-main');
+const tripControlsElem = siteMainElem.querySelector('.trip-main__trip-controls');
+const tripControlsHeaderElem = tripControlsElem.querySelectorAll('h2')[1];
+console.log(tripControlsHeaderElem);
 
-const siteHeaderElem = document.querySelector('.trip-main__trip-controls');
 
 render(siteMainElem, createTripInfoTemplate(), `afterbegin`);
+render(tripControlsHeaderElem, createSiteMenuTemplate(), `beforebegin`);
+render(tripControlsElem, createFilterTemplate());
+
 
 const tripInfoElem = siteMainElem.querySelector('.trip-main__trip-info');
 

73be4356c0355635d539eb74048e767932709562
Yuriy <yourgen.edu@mail.ru>
2020-04-30 12:50:17 +0300

    доработка контроллера панели

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -43,14 +43,21 @@ const getSortedEvents = (events, sortingType) => {
 export default class TripController {
   constructor(container) {
     this._container = container;
+
+    this._events = [];
+    this._tripStart = null;
     this._sortingComponent = new Sorting();
     this._tripDaysComponent = new TripDays();
     this._noPointsComponent = new NoPoints();
+    this._onSortingTypeChange = this._onSortingTypeChange.bind(this);
+    this._sortingComponent.setSortingTypeChangeHandler(this._onSortingTypeChange);
   }
 
-  render(headerContainer, tripPoints, tripStart) {
+  render(headerContainer, events, tripStart) {
+    this._events = events;
+    this._tripStart = tripStart;
     const container = this._container;
-    if (tripPoints.length === 0) {
+    if (events.length === 0) {
       render(container, this._noPointsComponent);
       return;
     }
@@ -61,26 +68,27 @@ export default class TripController {
 
     const tripDaysElement = this._tripDaysComponent.getElement();
 
-    tripPoints.map((eventlist, dayCount) => {
+    events.map((eventlist, dayCount) => {
       render(tripDaysElement, new TripPoint(dayCount + 1, tripStart));
 
       renderEventList(eventlist, tripDaysElement, dayCount, tripStart);
     });
-
-    this._sortingComponent.setSortingTypeChangeHandler((sortingType) => {
-      const sortedEvents = getSortedEvents(tripPoints, sortingType);
-      tripDaysElement.innerHTML = ``;
-
-      if (sortingType === SortingType.DEFAULT) {
-        sortedEvents.map((eventlist, dayCount) => {
-          render(tripDaysElement, new TripPoint(dayCount + 1, tripStart));
-          renderEventList(eventlist, tripDaysElement, dayCount, tripStart);
-        });
-      } else {
-        const NO_DAYS = 0;
-        render(tripDaysElement, new TripPoint(NO_DAYS, tripStart));
-        renderEventList(sortedEvents, tripDaysElement, NO_DAYS, tripStart);
-      }
-    });
   }
+
+  _onSortingTypeChange(sortingType) {
+    const sortedEvents = getSortedEvents(this._events, sortingType);
+    const tripDaysElement = this._tripDaysComponent.getElement();
+    tripDaysElement.innerHTML = ``;
+
+    if (sortingType === SortingType.DEFAULT) {
+      sortedEvents.map((eventlist, dayCount) => {
+        render(tripDaysElement, new TripPoint(dayCount + 1, this._tripStart));
+        renderEventList(eventlist, tripDaysElement, dayCount, this._tripStart);
+      });
+    } else {
+      const NO_DAYS = 0;
+      render(tripDaysElement, new TripPoint(NO_DAYS, this._tripStart));
+      renderEventList(sortedEvents, tripDaysElement, NO_DAYS, this._tripStart);
+    }
+  };
 }
